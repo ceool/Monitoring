658507ad74872d9bfa82816f9ccf590d4c7b59bf
ceool <p1026jh@naver.com>
2021-04-27 11:33:01 +0900

    add Multi Path AppArray

--- promtail.sh
+++ promtail.sh
@@ -7,8 +7,9 @@ loki_port=3100
 batchwait="10s" #디폴트 1초
 service="ceool"
 pod="dev"
-stream="web_01"
-path="/var/log/*log"
+stream="was_01"
+appArr=("App1" "App2" "App3" "App4" "App5")
+path="/data/log" # 아래 __path__ 확인
 
 
 mkdir $CDIR/promtail
@@ -25,31 +26,34 @@ cat > $CDIR/promtail/promtail-local-config.yaml <<EOF
 server:
   http_listen_port: 9080
   grpc_listen_port: 0
-
 positions:
   filename: /tmp/positions.yaml
-
 clients:
   - url: http://${loki_ip}:${loki_port}/loki/api/v1/push
     batchwait: "${batchwait}"
-
 scrape_configs:
 - job_name: system
   static_configs:
-  - targets:
+EOF
+
+for app in "${appArr[@]}";
+do
+echo "  - targets:
       - localhost
     labels:
       job: varlogs
       container_name: ${service}
       pod_name: ${service}-${pod}
-      stream: ${service}-${pod}-${stream}
-      __path__: ${path}
-     # host: testserver
+      stream: ${service}-${pod}-${stream}-${app}
+      __path__: ${path}/${app}_log/*" >> $CDIR/promtail/promtail-local-config.yaml
+done
+
+echo "     # host: testserver
 #pipeline_stages:
 #  - timestamp:
 #    format: 2006/01/02 15:04:05.999 MST
-#    source: timestamp
-EOF
+#    source: timestamp" >> $CDIR/promtail/promtail-local-config.yaml
+
 
 touch /etc/systemd/system/promtail.service
 cat > /etc/systemd/system/promtail.service <<EOF
