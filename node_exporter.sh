caf0ec4d52163a2320eed0a8e8e4ece048f761c6
ceool <p1026jh@naver.com>
2021-05-12 17:25:59 +0900

    node_exporter v1.1.2 update

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
 CDIR=$(pwd)
+version="1.1.2"
 
-wget --directory-prefix=$CDIR  https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
-tar xvf $CDIR/node_exporter-1.0.1.linux-amd64.tar.gz -C $CDIR/
-rm $CDIR/node_exporter-1.0.1.linux-amd64.tar.gz
-mv $CDIR/node_exporter-1.0.1.linux-amd64 $CDIR/node_exporter
+wget --directory-prefix=$CDIR  https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.linux-amd64.tar.gz
 
-touch /etc/systemd/system/monitoring.service
-cat > /etc/systemd/system/monitoring.service <<EOF
+tar xvf $CDIR/node_exporter-*.tar.gz -C $CDIR/
+rm -rf $CDIR/node_exporter-*.tar.gz
+mv $CDIR/node_exporter-*.tar.gz
+
+touch /etc/systemd/system/node_exporter.service
+cat > /etc/systemd/system/node_exporter.service <<EOF
 [Unit]
 Description=node_exporter for Prometheus Monitoring
 After=network.target
 [Service]
-ExecStart=$CDIR/node_exporter/node_exporter
+ExecStart=$CDIR/node_exporter-$version.linux-amd64/node_exporter
 [Install]
 WantedBy=multi-user.target
 EOF
 
 systemctl daemon-reload
-systemctl start monitoring
-systemctl enable monitoring
-systemctl status monitoring
+systemctl start node_exporter
+systemctl enable node_exporter
+systemctl status node_exporter
